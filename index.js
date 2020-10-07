eee1802c11925dbd3be821253cf2afc75532cbfa
amyusup <amyusup26@gmail.com>
2020-10-08 02:37:19 +0700

    UPDATEFINAL

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use('/transfer', rTransfer)
 // var routes = require("./src/helper/routes");
 // routes(app);
 
-app.listen(8000,() =>{
+app.listen(process.env.PORT || 8000,() =>{
     console.log('Sever running on port 8000')
 });
 
